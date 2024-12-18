7c5b3b0d0f327b4671c32013c7b1c9c5613a75e7
jprammell <jprammell@yahoo.com>
2024-12-18 09:57:49 -0700

    created basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 8989
+const port = process.env.PORT || 8989;
 
-app.get('/', (request, response) => {
-    response.send("<h1>Hello This is our App!</h1>")
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello This is our App!</h1>");
+});
 
-app.get('/login', (request, response) => {
-    response.send("<h1>Hello This is our Login Page</h1>")
-})
+app.get("/login", (request, response) => {
+  response.send("<h1>Hello This is our Login Page</h1>");
+});
 
 app.listen(port, () => {
-    console.log(`The application is running on port http://localhost:${port}`)
-} )
+  console.log(`The application is running on port http://localhost:${port}`);
+});
